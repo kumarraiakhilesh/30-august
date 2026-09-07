edc2ce594aa4d15de3eab009c78432dc2ec63c08
kumarraiakhilesh <kumarraiakhilesh>
2026-09-07 13:28:04 +0530

    add Env and Module directories and update pipeline

---
+++ Env/Dev/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "akhileshtestpip1"
+    storage_account_name = "akhileshstoragepipi1"
+    container_name       = "akhileshcanttest1"
+    key                  = "pipe.tfstate1"
+    subscription_id      = "974dc80c-fc30-43c1-96a6-106db3c5d9c9"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.80.0"
+    }
+  }
+}
+provider "azurerm" {
+features {}
+subscription_id = "974dc80c-fc30-43c1-96a6-106db3c5d9c9"
+}
